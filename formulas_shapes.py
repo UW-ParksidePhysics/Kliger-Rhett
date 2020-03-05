73fd17b3e547d5b450300ae48d510fb3ba4f06af
klige002 <klige002@rangers.uwp.edu>
2020-03-05 15:42:28 -0600

    Move formulas to shapes to GitHub

--- formulas_shapes.py
+++ formulas_shapes.py
@@ -4,13 +4,13 @@ h=5.0  #height
 b=2.0  #base
 r=1.5  #radius
 area_parallelogram = h*b
-print ’The area of the parallelogram is %.3f’ % area_parallelogram
+print('The area of the parallelogram is {:.3f}'.format(h*b))
 
 area_square = b**2
-print ’The area of the square is %g’ % area_square
+print('The area of the square is {:g}'.format(area_square))
 
 area_circle = pi*r**2
-print ’The area of the circle is %.3f’ % area_circle
+print('The area of the circle is {:.3f}'.format(area_circle))
 
 volume_cone = 1.0/3*pi*r**2*h
-print ’The volume of the cone is %.3f’ % volume_cone
+print('The volume of the cone is {:.3f}'.format(volume_cone))
