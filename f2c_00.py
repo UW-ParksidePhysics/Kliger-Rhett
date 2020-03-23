baab5c5ba213c1172f8681ef1c8693d23fc898a5
ball_table1.py <klige002@rangers.uwp.edu>
2020-03-23 11:22:51 -0500

    Put f2c on github

---
+++ f2c_00.py
@@ -0,0 +1,11 @@
+# Formula for converting fahrenheit to celsius
+def C(F):
+    return 5/9*(F-32)
+temp1 = C(0)
+print(temp1)
+temp2 = C(32) # Freezing point of water
+print(temp2)
+temp3 = C(100)
+print(temp3)
+temp4 = C(68) # Room temperature
+print(temp4)
