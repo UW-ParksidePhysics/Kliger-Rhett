a033c59d774f58f63524f49f7267a98912e8c050
ball_table1.py <klige002@rangers.uwp.edu>
2020-03-23 15:26:51 -0500

    Put finding primes on github

---
+++ find_primes_00.py
@@ -0,0 +1,12 @@
+#Finding Primes
+def Sieve(n):
+    numbers = list(range(2,n+1))
+    primes = []
+    for p in range(len(numbers)):
+        if numbers[p] != 0:
+            primes.append(numbers[p])
+            for i in range(numbers[p], n + 1, numbers[p]):
+                numbers[i - 2] = 0
+    return primes
+primes100 = Sieve(100)
+print(primes100)
