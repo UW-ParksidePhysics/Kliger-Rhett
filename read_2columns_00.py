7b2e1754c6c6a9635e904d0ba9fc1e37118396c0
RhettKliger <60672184+RhettKliger@users.noreply.github.com>
2020-04-16 16:38:22 -0500

    Delete read_2columns_00.py

--- read_2columns_00.py
+++
@@ -1,8 +0,0 @@
-# Make a program that reads the first column into a list x and the second column into a list y.
-# Then convert the lists to arrays,and plot the curve.Print out the maximum and minimum y coordinates.
-
-
-
-
-x = []
-y = []
