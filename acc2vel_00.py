11425b2f4c35de8306aae9be6dd820fefb53d9c8
ball_table1.py <klige002@rangers.uwp.edu>
2020-04-21 16:34:00 -0500

    Put on github

--- acc2vel_00.py
+++ acc2vel_00.py
@@ -5,23 +5,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 101
-
+dt = 1
 
 def velocity(a, dt):
     n = len(a)
     v = np.zeros(n)
-    for k in np.xrange(1, n):
-        v[k] =v[k−1] + 0.5*dt*(a[k−1] + a[k])  #How to make this work
-        v *= dt
-        return v
+    for k in np.arange(1, n):
+        v[k] = v[k-1] + dt*0.5*(a[k-1] + a[k])  # Is the use of brackets accurate for what we want to do here?
+    return v
 
-    acc = np.loadtxt('acc.dat_00.py', 'r')
-    vel = velocity(acc, dt)
-    time = np.array([i * dt for i in range(0, len(acc))])
 
-    plt.plot(time, acc, color='green', linewidth=1.5)
-    plt.plot(time, vel, color='blue', linewidth=1.5)
-    plt.xlabel('Time')
-    plt.legend(['Acceleration', 'Velocity'], loc=2)
-    plt.title('Time dependence of object acceleration and velocity')
-    plt.show()
+acc = np.loadtxt('acc.dat_00.py')
+
+vel = velocity(acc, dt)
+time = np.array([i * dt for i in range(0, len(acc))])
+
+plt.plot(time, acc, color='green', linewidth=1.5)
+plt.plot(time, vel, color='blue', linewidth=1.5)
+plt.xlabel('Time')
+plt.legend(['Acceleration', 'Velocity'], loc=2)
+plt.title('Time dependence of object acceleration and velocity')
+plt.show()
+
+#  Success
--- fundemental_constants_00.py
+++ fundemental_constants_00.py
@@ -1,14 +1,24 @@
 # Make a function that reads and interprets the text in the file, and there after returns the dictionary.
 
 
-def read_constants('constants_00.py'):
-    infile = open('constants_00.py', 'r')
+def read_constants(filename):
+    infile = open(filename, 'r')
     constants = {}
+    infile.readline()
+    infile.readline()
     for line in infile:
-        words = line.split()
-        value = float(words(-1))
+        name_of_constant = ' '.join(line.split()[:-2]) # Forgive me if you already said this but why -2, value of constant is second from the right. : end at that point
+
+        value = (line.split()[-2])
+
+        constants[name_of_constant] = value  # Constants is dictionary, name of constant is the keyword, the value is what we are looking for Sorry i didn't hear that
+
     infile.close()
+
     return constants
+
+
+constants = read_constants('constants_00.py')
 print('--------------------------------')
-print(constants, value)
+print(constants)
 print('--------------------------------')
