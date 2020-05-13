93b7e32fddea19e0171168c39d75eab806ad8756
ball_table1.py <klige002@rangers.uwp.edu>
2020-05-13 14:56:53 -0500

    Put on github

--- Kliger_Rhett_PHYS241FinalScript.py
+++ Kliger_Rhett_PHYS241FinalScript.py
@@ -3,42 +3,52 @@
 
 # Overview
 import matplotlib.pyplot as plt
-import two_column_text_read_00
+import two_column_text_read_00 as tctr
 import plot_data_with_fit_00
 import lowest_eigenvectors_00
-import univariate_statistics_00, statistics
+import univariate_statistics_00 as us
 import fit_curve_array_00
 import quadratic_fit_00
 import equations_of_state
-# So from 1 on parse
+import matplotlib.pyplot as np
+
 filename = 'Ge.Fd-3m.GGA-PBEsol.volumes_energies.dat'
-
-# I tried to do the overview here so is it ok to remove this portion?
-data = two_column_text_read_00.two_column_text_read(filename)  #Reads the file of two column data or should
-print(data)
-exit()
-
-#plot_data_with_fit_00.plot_data_with_fit(data, fit_curve=2, data_format=0, fit_format=0)
-
-#lowest_eigenvectors_00.square_matrix()
-
-#lowest_eigenvectors_00.lowest_eigenvectors(square_matrix=10, number_of_eigenvectors=3)
-#print(lowest_eigenvectors_00.lowest_eigenvectors)
+display_Graph = True
+my_eos = 'birch-murnaghan'
+potential_name = 'square'
+number_of_dimensions = 100
+potential_parameter = 200
 
 
 # Fit an Equation of a State
+
+# 1. Extract the chemical symbol denoted by (CS), crystal symmetry symbol (CSS), density functional exchange-correlation approximation acronym denoted (AA) from your given data file's name.
+# Write a function in your main script file called parse_file_name to return these three strings from the file name.
 def parse_file_name(filename):
-    print("Ge.Fd-3m.GGA-PBEsol")  # Return from filename
-    two_column_text_read_00.two_column_text_read(filename)
-    print(two_column_text_read_00.two_column_text_read(filename)) #Data from the file
-    univariate_statistics_00.univariate_statistics(data) # Take the data from the file and pull out statistics
-    fit_curve_array_00.fit_curve_array(statistics)
-    equations_of_state.fit_eos(quadratic_coefficients=3)
+    to_parse = filename.split('.')
+    CS, CSS, AA = to_parse[0:3]
+    print(to_parse[0:3])
+    return CS, CSS, AA
+
+# 2. Read in data using two_column_text_read
+data = tctr.two_column_text_read(filename)
+print('Data Read In:')
+print(data)
+
+# 3. Divide by 2 because Fd3m
+data1 = data/2
+print(data1)
+
+# 4. Pull out statistics using univariate_statistics module
+statistics = us.univariate_statistics(filename)
+print('Statistics of Data from File:')
+print(statistics)
+
 
 # Bohr is unit of length, Rydberg is unit of energy,
 # First column is volumes, second column is energies
-def convert_units(data):
-    return data[0]/data[1]  # Data 0 is volumes, data 1 is energies
+#def convert_units(data):
+    #return data[0]/data[1]  # Data 0 is volumes, data 1 is energies
 
 
 
