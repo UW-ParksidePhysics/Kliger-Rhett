d32efe4dac63d1715a524f5a56636dcab7cbb08c
ball_table1.py <klige002@rangers.uwp.edu>
2020-05-14 10:06:27 -0500

    Put on github

--- Kliger_Rhett_PHYS241FinalScript.py
+++ Kliger_Rhett_PHYS241FinalScript.py
@@ -2,6 +2,7 @@
 #Rhett Kliger
 
 # Overview
+
 import matplotlib.pyplot as plt
 import two_column_text_read_00 as tctr
 import plot_data_with_fit_00
@@ -10,7 +11,8 @@ import univariate_statistics_00 as us
 import fit_curve_array_00
 import quadratic_fit_00 as qf
 import equations_of_state as eos
-import matplotlib.pyplot as np
+import statistics as st
+import numpy as np
 
 filename = 'Ge.Fd-3m.GGA-PBEsol.volumes_energies.dat'
 display_Graph = True
@@ -72,13 +74,13 @@ def convert_units(value_to_convert_from, units_of_value_converted_from, unit_to_
     elif units_of_value_converted_from == 'rydberg/cubic bohr':
         value_in_requested_units = value_to_convert_from / 29421.0265
     else:
-        value_in_requested_units = value_to_convert_from
+        value_in_requested_units = unit_to_convert_to
     return value_in_requested_units
 
 # 8. Plot the data and the fit function with volume on the horizontal axis and energy on the vertical axis
 # Need to make it so that max x values and min x values are 10% beyond max and min data points
 # You should use raw strings (precede the quotes with an 'r'), and surround the math text with dollar signs ($)
-volumes = plt.linspace(min(data1[0], max(data1[0]), len(eos_passed_info)))
+volumes = np.linspace(min(data1[0], max(data1[0]), len(eos_passed_info)))
 print(volumes)
 plt.plot(data[0], data[1], 'o', color='blue')
 plt.plot(volumes, eos_passed_info, color='black')
@@ -91,16 +93,17 @@ ylabel = (r' $E$ $ \mathrm{AA^3/atom} $')
 plt.show()
 
 # 9. Write a function called annotate_graph to annotate the graph
-xlocation1 = (min(data1[0]))  # Upper lefthand corner
-ylocation1 = ('Height above graph')  # I will need to figure this out after I get previous to work, placeholder
-ylocation2 = ('Height above ylocation1')  # I will need to figure this out after I get previous to work, placeholder
-def annotate_graph(data1, eos_passed_info):
-    Ge = plt.text((min(data1[0])), max(eos_passed_info), 'Ge')
-    GGA_PBEsol = plt.text(xlocation1, ylocation1, (r' $Fd-3m\mathit{GGA-PBEsol}\ $'))
-    K = plt.text(xlocation1, ylocation2, ('K_0 =' + bulk_modulus + 'GPa'))
-    return Ge, GGA_PBEsol, K
+# 'Numbers' are placeholders
+equilibrium_volume = st.mean(eos_passed_info)
+def annotate_graph(PLACEHOLDER):
+    Ge = plt.text('number','number', 'Ge')
+    GGA_PBEsol = plt.text('number', 'number', (r' $Fd-3m\mathit{GGA-PBEsol}\ $'))
+    K = plt.text('number', 'number', ('K_0 =' + bulk_modulus + 'GPa'))
+    EqV = plt.text('number', 'number', 'V_0 =' + equlibrium_volume + r' $\mathit{AA^3/atom}\ $')
+    signature = ax.text(left, bottom, 'Created By Rhett Kliger 2020-5-14', horizontalalignment='left', verticalalignment='top', transform=ax.transAxes)
+    return Ge, GGA_PBEsol, K, EqV,
 
-
+# 10.
 
 
 
